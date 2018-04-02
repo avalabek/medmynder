9b56940899a63d322baed838cb04652e9eac9e65
Andreya <avalabek@yahoo.com>
2018-04-02 08:08:56 -0700

    changes in code do not render

--- app/components/Account/Signup.js
+++ app/components/Account/Signup.js
@@ -41,30 +41,41 @@ class Signup extends React.Component {
   }
 
   render() {
-    return (
-      <div className="column row">
+    return <div className="column row" style={{ backgroundColor: "lime" }}>
         <div className="row">
           <div className="medium-8 medium-offset-2 columns">
-            <Messages messages={this.props.messages}/>
+            <Messages messages={this.props.messages} />
             <form onSubmit={this.handleSignup.bind(this)}>
-              <h4>Create an account</h4>
+              <h4>Create an account and let's see if it renders</h4>
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" placeholder="Name" value={this.state.name} onChange={this.handleChange.bind(this)} autoFocus/>
+              <input type="text" name="name" id="name" placeholder="Name" value={this.state.name} onChange={this.handleChange.bind(this)} autoFocus />
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" placeholder="Email" value={this.state.email} onChange={this.handleChange.bind(this)}/>
+              <input type="email" name="email" id="email" placeholder="Email" value={this.state.email} onChange={this.handleChange.bind(this)} />
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="password" placeholder="Password" value={this.state.password} onChange={this.handleChange.bind(this)}/>
-              <p className="help-text">By signing up, you agree to the <Link to="/">Terms of Service</Link>.</p>
-              <button type="submit" className="button">Create an account</button>
+              <input type="password" name="password" id="password" placeholder="Password" value={this.state.password} onChange={this.handleChange.bind(this)} />
+              <label htmlFor="dateofbirth">Date of birth</label>
+              <input type="text" name="dateofbirth" id="dateofbirth" placeholder="Date of birth" value={this.state.dateofbirth} onChange={this.handleChange.bind(this)} autoFocus />
+              <label htmlFor="phone">Phone</label>
+              <input type="text" name="phone" id="phone" placeholder="Phone" value={this.state.phone} onChange={this.handleChange.bind(this)} autoFocus />
+              <p className="help-text">
+                By signing up, you agree to the <Link to="/">
+                  Terms of Service
+                </Link>.
+              </p>
+              <button type="submit" className="button">
+                Create an account
+              </button>
             </form>
-            <div className="hr-title"><span>or</span></div>
-            <div className="button-group">
+            <div className="hr-title">
+              <span>or</span>
             </div>
-            <p>Already have an account? <Link to="/login">Log in</Link></p>
+            <div className="button-group" />
+            <p>
+              Already have an account? <Link to="/login">Log in</Link>
+            </p>
           </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
